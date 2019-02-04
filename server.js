9a0712d2f40cc12efdfff0539d9d9fffab914e31
Fadli Ramadhan <fadlir93@gmail.com>
2019-02-05 06:48:16 +0700

    Test Server

--- server.js
+++ server.js
@@ -6,3 +6,11 @@ const PORT = process.env.PORT || 3000 ;
 const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+    res.send("Welcome to app invoicing App");
+})
+
+app.listen(PORT, function() {
+    console.log(`App Running on PORT : ${PORT}`)
+})
